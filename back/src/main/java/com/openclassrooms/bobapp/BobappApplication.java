2145f2bb98f4819fa0edd10ace4588fa99f88252
Gilles Bernard <gilles.bd@free.fr>
2024-03-22 17:00:41 +0100

    Javadoc added

--- back/src/main/java/com/openclassrooms/bobapp/BobappApplication.java
+++ back/src/main/java/com/openclassrooms/bobapp/BobappApplication.java
@@ -3,6 +3,9 @@ package com.openclassrooms.bobapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * BobApp application main class.
+ */
 @SpringBootApplication
 public class BobappApplication {
 
